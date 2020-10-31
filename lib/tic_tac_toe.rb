c39b4dc0fdc499064b386edd1b27f596a488a05b
Matt Smith <mrandolph83@gmail.com>
2020-10-31 02:11:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -12,9 +12,9 @@ WIN_COMBINATIONS = [
 
 ]
 
-  def initialize(board = nil)
+def initialize(board = nil)
     @board = board || Array.new(9, " ")
-  end
+end
 
 def display_board
   puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
